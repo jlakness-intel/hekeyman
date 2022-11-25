df71544bb70ea1b2ed7a4bb00de7f99f82a11f4c
John Lakness <84150418+jlakness-intel@users.noreply.github.com>
2022-11-24 17:11:08 -0800

    single-shot generation and use actual noise

--- test/gen.cpp
+++ test/gen.cpp
@@ -9,26 +9,30 @@
 using namespace std;
 using namespace helib;
 
-typedef array<uint32_t,32/sizeof(uint32_t)> Seed;
+typedef array<uint32_t,16/sizeof(uint32_t)> Seed;
+
+zzX genPoly(long phim,Seed seed){
+    zzX poly;
+    poly.SetLength(phim);
+    seed_seq seq(seed.begin(),seed.end());
+    vector<int32_t> vals(phim,0);
+    seq.generate(vals.begin(),vals.end());
+    transform(vals.begin(),vals.end(),poly.begin(),[](int32_t v)->double{
+        return v/(INT32_MAX/2);
+        });
+    return poly;
+}
 
 SecKey genSK(Context& ctx, Seed seed){
     long phim = ctx.getPhiM();
     double bound = sqrt(phim * log(phim) / 2.0);
-    zzX poly;
-    poly.SetLength(phim);
-    seed_seq seq(seed.begin(),seed.end());
-    do {
-        vector<int32_t> vals(phim,0);
-        seq.generate(vals.begin(),vals.end());
-        transform(vals.begin(),vals.end(),poly.begin(),[](int32_t v)->double{
-            return v/(INT32_MAX/2);
-            });
-    } while (
-        embeddingLargestCoeff(poly, ctx.getZMStar()) > bound
-    );
+    zzX poly = genPoly(ctx.getPhiM(),seed);
+    double noise = embeddingLargestCoeff(poly, ctx.getZMStar());
+    if (noise>bound) 
+        cerr << "Warning: noise " << noise << " above bound " << bound << endl;
     DoubleCRT dcrt(poly, ctx,ctx.getCtxtPrimes()|ctx.getSpecialPrimes());
     SecKey sk(ctx);
-    sk.ImportSecKey(dcrt,bound);
+    sk.ImportSecKey(dcrt,noise);
     return sk;
 }
 
@@ -40,7 +44,8 @@ string SKtoJSON(SecKey& sk){
 
 int main(int argc, char* argv[])
 {
-    Context ctx = ContextBuilder<CKKS>().m(256).bits(32).precision(16).c(2).build();
+    //Context ctx = ContextBuilder<CKKS>().m(256).bits(32).precision(16).c(2).build();
+    Context ctx = ContextBuilder<CKKS>().m(16*1024).bits(119).precision(20).c(2).build();
     random_device rd;
     Seed seed;
     for(auto& s:seed) s=rd();
@@ -49,13 +54,11 @@ int main(int argc, char* argv[])
     string s1 = SKtoJSON(sk1);
     string s2 = SKtoJSON(sk2);
     assertEq(s1,s2,"deterministic key generation failed");
-    cout<<endl<<s1<<endl<<endl<<s2<<endl;
 
     vector<double> vals(ctx.getNSlots()),v1,v2;
     generate(vals.begin(),vals.end(),[](){
         return static_cast<double>(rand())/RAND_MAX;
         });
-    cout<<vals<<endl;
 
     PtxtArray pt(ctx,vals),pt1(ctx),pt2(ctx);
     Ctxt ct1(sk1),ct2(sk2);
@@ -65,8 +68,6 @@ int main(int argc, char* argv[])
     pt2.decrypt(ct2,sk2);
     pt1.store(v1);
     pt2.store(v2);
-
-    cout<<v1<<endl<<v2<<endl;
 
     double sse = inner_product(v1.begin(),v1.end(),v2.begin(),0.0,plus(),[](double a,double b)->double {
         double d = a-b;
